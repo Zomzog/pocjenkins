4ca1957f0635279048763ec5e136e625288bb621
Thibault Duperron <thibault.duperron@orange.com>
2018-01-12 15:09:41 +0100

    init springboot

---
+++ src/main/java/bzh/oab/jenkinspoc/poc/jenkins/PocJenkinsApplication.java
@@ -0,0 +1,12 @@
+package bzh.oab.jenkinspoc.poc.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJenkinsApplication.class, args);
+	}
+}
